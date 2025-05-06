b195b31d457e8075e7e47887684a4a9d5c0fe894
markli1hoshipu <marklizhiyuan@outlook.com>
2025-05-05 21:18:29 -0400

    deleted unneeded files

--- src/tabs/__init__.py
+++ src/tabs/__init__.py
@@ -2,29 +2,13 @@
 
 from .base_tab import BaseTab
 from .predict_tab import PredictTab
-from .manual_tab import ManualTab
 from .adb_tab import ADBTab
 from .opencv_tab import OpenCVTab
-from .train_tab import TrainTab
-from .legacy_tab import LegacyTab
 from .photopredict_tab import PhotoPredictTab
 __all__ = [
     'BaseTab',
     'PredictTab',
-    'ManualTab',
     'ADBTab',
     'OpenCVTab',
-    'TrainTab', 
-    'LegacyTab'
+    'PhotoPredictTab'
 ]
-
-"""
-战斗预测系统 - 标签页模块
-
-包含以下标签页实现：
-- PredictTab: 预测功能页
-- ManualTab: 手动录入页
-- ADBTab: ADB录入页
-- OpenCVTab: OpenCV录入页
-- LegacyTab: 旧版UI页
-"""
